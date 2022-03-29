1213141f9e7e1e20be875b1baec5157108bf3d0c
TevLee <ltwy0311@naver.com>
2022-03-29 18:12:53 +0900

    feat : lookup categories

---
+++ src/main/java/bebeShare/service/CategoryService.java
@@ -0,0 +1,19 @@
+package bebeShare.service;
+
+import bebeShare.domain.code.CodeRepository;
+import bebeShare.web.dto.CodeResponseDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+    private final CodeRepository codeRepository;
+
+    public List<CodeResponseDto> lookup(String code){
+        System.out.println("CategoryService.lookup");
+        return codeRepository.lookup(code);
+    }
+}
