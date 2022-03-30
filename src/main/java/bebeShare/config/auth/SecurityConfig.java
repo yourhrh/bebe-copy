1048f5e518cfaa74dcf6cfcf101d18e8bc4a2bd7
superkkj <gabunot@gmail.com>
2022-03-30 10:52:39 +0900

    chore: 깃 out파일 삭제관련 ㅋ ㅓ밋

--- src/main/java/bebeShare/config/auth/SecurityConfig.java
+++ src/main/java/bebeShare/config/auth/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-//                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
                 .logout()
