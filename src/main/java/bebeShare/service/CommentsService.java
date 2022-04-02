80e8f13983f2cc103e1ae5e33c8ac601f17c6bb2
TevLee <ltwy0311@naver.com>
2022-04-03 00:11:19 +0900

    chore : add dto

--- src/main/java/bebeShare/service/CommentsService.java
+++ src/main/java/bebeShare/service/CommentsService.java
@@ -13,41 +13,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
-//@Service
-//@RequiredArgsConstructor
-//public class CommentsService {
-//    private final CommentRepository commentRepository;
-//    private final UserRepository userRepository;
-//    private final ProductRepository productRepository;
-//
-//
-//    @Transactional
-//    public CommentResponseDto save(CommentSaveRequestsDto requestsDto) {
-//
-//        User user = userRepository.findById(requestsDto.getMemberId())
-//                .orElseThrow( () -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR));
-//        Product product = productRepository.findById(requestsDto.getProductId())
-//                .orElseThrow(() -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR));
-//
-//        commentRepository.save(requestsDto.toEntity(user, product));
-//
-//        return new CommentResponseDto(requestsDto.getProductId());
-//    }
-//
-//    @Transactional
-//    public CommentUpdateResponseDto update(Long productId, Long commentId, CommentUpdateRequestsDto updateRequestsDto) {
-//            Comment entity = commentRepository.findById(commentId).orElseThrow(
-//                    ()-> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR)
-//            );
-//            entity.update(commentId,updateRequestsDto);
-//        return new CommentUpdateResponseDto(productId);
-//    }
-//
-//    @Transactional
-//    public CommentDeleteResponseDto delete(Long productId, Long commentId) {
-//        commentRepository.deleteById(commentId);
-//        return new CommentDeleteResponseDto(productId);
-//    }
-//}
+
+@Service
+@RequiredArgsConstructor
+public class CommentsService {
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+
+
+    @Transactional
+    public CommentResponseDto save(CommentSaveRequestsDto requestsDto) {
+
+        User user = userRepository.findById(requestsDto.getMemberId())
+                .orElseThrow(() -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR));
+        Product product = productRepository.findById(requestsDto.getProductId())
+                .orElseThrow(() -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR));
+
+        commentRepository.save(requestsDto.toEntity(user, product));
+
+        return new CommentResponseDto(requestsDto.getProductId());
+    }
+
+    @Transactional
+    public CommentUpdateResponseDto update(Long productId, Long commentId, CommentUpdateRequestsDto updateRequestsDto) {
+        Comment entity = commentRepository.findById(commentId).orElseThrow(
+                () -> new CustomException(ErrorCode.INTERNAL_SERVER_ERROR)
+        );
+        entity.update(commentId, updateRequestsDto);
+        return new CommentUpdateResponseDto(productId);
+    }
+
+    @Transactional
+    public CommentDeleteResponseDto delete(Long productId, Long commentId) {
+        commentRepository.deleteById(commentId);
+        return new CommentDeleteResponseDto(productId);
+    }
+}
