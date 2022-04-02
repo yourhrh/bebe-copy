559b6434295313db76f47a6a09181f8f991d9a08
omnireader0 <omnireader0@gmail.com>
2022-04-02 23:41:18 +0900

    feat : update and delete dibs(like)

--- src/main/java/bebeShare/domain/like/DibsRepository.java
+++ src/main/java/bebeShare/domain/like/DibsRepository.java
@@ -1,9 +1,13 @@
 package bebeShare.domain.like;
 
 
+import bebeShare.domain.product.Product;
+import bebeShare.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DibsRepository extends JpaRepository<Dibs, Long> {
+import java.util.Optional;
 
+public interface DibsRepository extends JpaRepository<Dibs, Long> {
+    Optional<Dibs> findByUserAndProduct(User user, Product product);
 
 }
