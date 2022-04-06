3e17b1098e165b4296150a594e9f64fa1d09449d
superkkj <gabunot@gmail.com>
2022-04-06 18:11:46 +0900

    refactor: 코드 리팩토링

---
+++ src/main/java/bebeShare/domain/common/CommonContants.java
@@ -0,0 +1,8 @@
+package bebeShare.domain.common;
+
+public class CommonContants {
+
+    public static final String COMMENT_SAVE_OK = "댓글 등록 성공";
+    public static final String COMMENT_UPDATE_OK = "댓글 수정 성공";
+    public static final String COMMENT_DELETE_OK = "댓글 삭제 성공";
+}
